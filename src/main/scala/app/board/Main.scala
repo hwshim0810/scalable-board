77bae1f91987678e3e8f45f542085fd8b8d8764a
hwshim0810 <shim.hw0810@gmail.com>
2017-07-16 14:01:49 +0900

    ADD] 메인페이지 로드 추가

--- src/main/scala/app/board/Main.scala
+++ src/main/scala/app/board/Main.scala
@@ -1,15 +1,16 @@
 package app.board
 
-import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
-import akka.stream.ActorMaterializer
 import scala.io.StdIn
 
-import scala.concurrent.Future
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server._
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
 
-object WebServer {
+
+
+object Main {
   def main(args: Array[String]) {
 
     implicit val system = ActorSystem("board-sys")
@@ -17,20 +18,37 @@ object WebServer {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
-    val route =
-      path("board") {
+    val route: Route =
+      pathSingleSlash {
         get {
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+          getFromResource("static/main.html")
+        }
+      } ~ {
+        getFromResourceDirectory("static")
+      } ~
+      pathPrefix("board") {
+        path(IntNumber / "info") { page =>
+          get {
+            complete {
+              "info : " + page
+            }
+          }
+        } ~
+        path("articles" / IntNumber / "info") { articleid =>
+          get {
+            complete {
+              "article : " + articleid
+            }
+          }
         }
       }
 
+    // `route` will be implicitly converted to `Flow` using `RouteResult.route2HandlerFlow`
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
-
   }
 }
