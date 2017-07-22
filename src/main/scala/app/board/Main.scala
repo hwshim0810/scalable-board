50756c33e36d12470a7e27d9cf79eae12918f228
hwshim0810 <shim.hw0810@gmail.com>
2017-07-22 16:42:04 +0900

    MOD] Main 수정

--- src/main/scala/app/board/Main.scala
+++ src/main/scala/app/board/Main.scala
@@ -1,54 +1,31 @@
 package app.board
 
-import scala.io.StdIn
-
 import akka.actor.ActorSystem
+import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.server._
-import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import app.board.http.HttpService
+import app.board.services.{AuthService, UsersService, ArticlesService}
+import app.board.utils.{Config, DatabaseService, FlywayService}
 
+import scala.concurrent.ExecutionContext
 
+object Main extends App with Config {
+  implicit val actorSystem = ActorSystem()
+  implicit val executor: ExecutionContext = actorSystem.dispatcher
+  implicit val log: LoggingAdapter = Logging(actorSystem, getClass)
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
 
-object Main {
-  def main(args: Array[String]) {
+  val flywayService = new FlywayService(jdbcUrl, dbUser, dbPassword)
+  flywayService.migrateDatabaseSchema
 
-    implicit val system = ActorSystem("board-sys")
-    implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext = system.dispatcher
+  val databaseService = new DatabaseService(jdbcUrl, dbUser, dbPassword)
 
-    val route: Route =
-      pathSingleSlash {
-        get {
-          getFromResource("static/main.html")
-        }
-      } ~ {
-        getFromResourceDirectory("static")
-      } ~
-      pathPrefix("board") {
-        path(IntNumber / "info") { page =>
-          get {
-            complete {
-              "info : " + page
-            }
-          }
-        } ~
-        path("articles" / IntNumber / "info") { articleid =>
-          get {
-            complete {
-              "article : " + articleid
-            }
-          }
-        }
-      }
+  val usersService = new UsersService(databaseService)
+  val authService = new AuthService(databaseService)(usersService)
+  val articlesService = new ArticlesService(databaseService)
 
-    // `route` will be implicitly converted to `Flow` using `RouteResult.route2HandlerFlow`
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
-  }
+  val httpService = new HttpService(usersService, authService, articlesService)
+
+  Http().bindAndHandle(httpService.routes, httpHost, httpPort)
 }
